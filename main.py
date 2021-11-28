28217a42444820a8c69b837fcacf1c02641ccd68
locadm <locadm.green@gmail.com>
2021-11-28 11:49:23 +0100

    message packets done message headers done packetdatabase WIP

--- main.py
+++ main.py
@@ -1,47 +1,111 @@
 import time
 import socket
 import threading
+from crc import CrcCalculator, Crc32
+crc_calculator = CrcCalculator(Crc32.CRC32)
 console_lock = threading.Lock()
 
 
+class PacketDatabase:
+    def __init__(self):
+        self.comms = []
+
+
+class Comm:
+    def __init__(self, com_ip, com_port):
+        self.ip = ip
+        self.port = port
+        self.packets = []
 
 class Packet:
-    def __init__(self, flag, order, crc, data):
+    def __init__(self, flag=b'0000', order=b'000000', crc=b'00000000', data=None):
         self.flag = flag
         self.order = order
         self.crc = crc
         self.data = data
 
+    def raw(self):
+        string = self.flag + self.order + self.crc
+        if self.data is not None:
+            string += self.data
+        return string
+
+
+def create_packet_flag(syn=False, size=False, name=False, psh=False, done=False, binary=False, txt=False,
+                       file=False, msg=False, nnod=False, nod=False):
+
+    parameters = locals().values()
+    final_flag = 0
+    bit_iterator = 1
+    for parameter in parameters:
+        if parameter:
+            final_flag += bit_iterator
+            print(bit_iterator)
+        bit_iterator *= 2
+
+    return "{0:0{1}x}".format(final_flag, 4).encode()
+
+def decypher_packet_flag(flag_bytes):
+    bit_value_iter = 65536
+
+    flag_dict = {"NOD": False,"NNOD": False,"MSG": False,"FILE": False,"TXT": False,"BIN": False,"DONE": False,
+                "PSH": False,"NAME": False,"SIZE": False,"SYN": False
+                 }
+    dict_keys = ["NOD", "NNOD", "MSG", "FILE", "TXT", "BIN", "DONE", "PSH", "NAME", "SIZE", "SYN"]
+    keys_iter = 10-16
+
+    flag = int(flag_bytes, 16)
+
+    for i in range(17):
+        if flag - bit_value_iter >= 0:
+            flag = flag - bit_value_iter
+            flag_dict[dict_keys[keys_iter]] = True
+        keys_iter += 1
+        bit_value_iter /= 2
+
+    return flag_dict
 
 
 def fragment_message(message: bytes) -> list:
     global fragment_size
     return [message[i:i + fragment_size] for i in range(0, len(message), fragment_size)]
 
-def create_message_packets(packets: list) -> list:
 
+def create_message_packets(fragments: list) -> list:
+    packets = []
+    i = 1
 
-def send_mode():
-    pass
+    header_flag = create_packet_flag(msg=True, size=True)
+    packets.append(Packet(flag=header_flag))
 
+    header_flag = create_packet_flag(psh=True, txt=True, msg=True)
+    for fragment in fragments:
+        checksum = "{0:0{1}x}".format(crc_calculator.calculate_checksum(fragment), 8).encode()
+        new_packet = Packet(flag=header_flag, crc=checksum, data=fragment)
+        new_packet.order = "{0:0{1}x}".format(i, 6).encode()
+        i += 1
+        packets.append(new_packet)
+
+    packets[-1].flag = create_packet_flag(psh=True, txt=True, msg=True, done=True)
+    packets[0].order = "{0:0{1}x}".format(len(packets), 6).encode()
+
+    return packets
+
+def assembler(given_data, given_addr):
+    global data
+
+    flags = decypher_packet_flag(data[:4])
+    if flags["SIZE"] == True and flags["TXT"] == True:
 
 def recieve_mode():
     global ip, port, sock
     while True:
-        data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+        data, addr = sock.recvfrom(1550)  # buffer size is 1024 bytes
+        ass = threading.Thread(target=assembler, args=(data, addr), daemon=True)
+
         console_lock.acquire()
         print(addr, data)
-        #sock.sendto("yooo".encode(), addr)
         console_lock.release()
-
-
-
-def switch_mode():
-    global mode
-    if mode == "send":
-        mode = "receive"
-    else:
-        mode = "send"
 
 
 def send_file(args):
@@ -56,17 +120,20 @@ def send_msg(args):
     global sock
 
     msg_frag = fragment_message(arguments[2].encode())
+    msg_frag = create_message_packets(msg_frag)
     for frag in msg_frag:
-        sock.sendto(frag, (arguments[0], int(arguments[1])))
+        sock.sendto(frag.raw(), (arguments[0], int(arguments[1])))
 
 
-
+def send_file(args):
+    pass
 
 def listen():
     pass
 
 
 def hub():
+
 
     listener = threading.Thread(target=recieve_mode, args=(), daemon=True)
     listener.stopped = False
@@ -76,10 +143,6 @@ def hub():
     while True:
         global ip, port, mode
         command = input("INPUT COMMANDS:\n").split(" ", maxsplit=1)
-
-        if command[0].lower() == "mode" or command[0].lower() == "mo":
-            switch_mode()
-            print(mode)
 
         if command[0].lower() == "socket" or command[0].lower() == "s":
             socket = command[1].split(" ")
@@ -119,6 +182,7 @@ if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((ip, int(port)))
+    packet_database = PacketDatabase()
 
     hub()
     # alicemessage = "Chapter One – Down the Rabbit Hole: Alice, a seven-year-old girl, is feeling bored and drowsy while sitting on the riverbank with her elder sister. She notices a talking, clothed white rabbit with a pocket watch run past. She follows it down a rabbit hole where she suddenly falls a long way to a curious hall with many locked doors of all sizes. She finds a little key to a door too small for her to fit through, but through it, she sees an attractive garden. She then discovers a bottle on a table labelled DRINK ME, the contents of which cause her to shrink too small to reach the key which she had left on the table. She subsequently eats a cake labelled  in currants as the chapter closes.Chapter Two – The Pool of Tears: The chapter opens with Alice growing to such a tremendous size that her head hits the ceiling. Unhappy, Alice begins to cry and her tears literally flood the hallway. After she picks up a fan that causes her to shrink back down, Alice swims through her own tears and meets a mouse, who is swimming as well. Alice, thinking he may be a French mouse, tries to make small talk with him in elementary French. Her opening gambit Où est ma chatte? (transl.Where is my cat?), however, offends the mouse, who then tries to escape her.Chapter Three – The Caucus Race and a Long Tale: The sea of tears becomes crowded with other animals and birds that have been swept away by the rising waters. Alice and the other animals convene on the bank and the question among them is how to get dry again. Mouse gives them a very dry lecture on William the Conqueror. A dodo decides that the best thing to dry them off would be a Caucus-Race, which consists of everyone running in a circle with no clear winner. Alice eventually frightens all the animals away, unwittingly, by talking about her (moderately ferocious) cat.Chapter Four – The Rabbit Sends a Little Bill: White Rabbit appears again in search of the Duchess's gloves and fan. Mistaking her for his maidservant, Mary Ann, Rabbit orders Alice to go into the house and retrieve them. Inside the house she finds another little bottle and drinks from it, immediately beginning to grow again. The horrified Rabbit orders his gardener, Bill the Lizard, to climb on the roof and go down the chimney. Outside, Alice hears the voices of animals that have gathered to gawk at her giant arm. The crowd hurls pebbles at her, which turn into little cakes. Alice eats them, and they reduce her again in size.Chapter Five – Advice from a Caterpillar: Alice comes upon a mushroom and sitting on it is a blue caterpillar smoking a hookah. Caterpillar questions Alice, who begins to admit to her current identity crisis, compounded by her inability to remember a poem. Before crawling away, the caterpillar tells Alice that one side of the mushroom will make her taller and the other side will make her shorter. She breaks off two pieces from the mushroom. One side makes her shrink smaller than ever, while another causes her neck to grow high into the trees, where a pigeon mistakes her for a serpent. With some effort, Alice brings herself back to her normal height. She stumbles upon a small estate and uses the mushroom to reach a more appropriate height. "
